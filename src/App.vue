85821e1d3e2fc880c4c7e651245380d26735366b
Moses Kamau <moseskamau338@gmail.com>
2023-05-30 12:12:13 +0300

    feat: create page section with bounded drag area

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,68 @@
 <script setup>
-// import PanelArea from "./components/PanelArea.vue";
-// import PanelEditor from "./components/Panel/PanelEditor.vue";
+import {TabGroup, TabList, TabPanels, TabPanel, Tab} from "@headlessui/vue";
+import {ref} from "vue";
 import PanelArea from "./components/PanelArea.vue";
-import PanelEditor from "./components/Panel/PanelEditor.vue";
+
+ const pages = ref([
+      {type:'header', name:'Pages'},
+      {type:'menu', name:'Accounts Metrics', disabled: false},
+      {type:'menu', name:'Transactions Overview',disabled: false},
+    ])
+
+const newPageName = ref('')
+
+
+const showAddPageInput = ref(false)
+
+const vFocus = {
+  mounted: (el) => el.focus()
+}
+
+const addPage = () => {
+     if(newPageName.value.length <= 0) return;
+
+     pages.value.push(
+         {type:'menu', name: newPageName.value, disabled: false},
+     )
+    newPageName.value = ''
+    showAddPageInput.value = false
+}
+
 </script>
 
 <template>
-    <section class="mx-20 py-4 h-screen">
-        <div class="flex items-center justify-between mt-5">
-            <h2>Build Dashboard</h2>
-            <div>
-                <button class="border border-slate-600 p-1 rounded px-3 flex justify-center items-center hover:bg-slate-200 focus:ring-1 focus:ring-slate-400 focus:ring-offset-1 focus:ring-offset-slate-100">+ Add Panel</button>
-            </div>
-        </div>
-        <div class="mt-20 h-full w-full relative">
-            <PanelEditor />
-        </div>
-    </section>
+<section class="mx-20 py-4 h-screen">
+    <TabGroup as="section" class="grid gap-6 grid-cols-1 lg:grid-cols-12 mt-8" vertical>
+        <TabList as="div" class="col-span-3 flex flex-col text-sm space-y-2 focus:outline-0">
+          <template v-for="item in pages">
+            <span v-if="item.type === 'header'" class="font-bold text-slate-400 pt-1">{{ item.name }}</span>
+            <Tab :disabled="item.disabled" v-slot="{ selected }" v-else-if="item.type === 'menu'" class="focus:outline-0 w-7/12">
+              <button :class="[
+                  selected ? 'bg-white shadow' : '',
+                  item.disabled ? 'opacity-70 pointer-events-none' : ''
+                ]"
+                 class="rounded text-left px-2 py-1.5 focus:border-none focus:ring-0 hover:bg-white w-full text-slate-500 focus:outline-0 transition-all duration-500 whitespace-nowrap truncate">
+                <i class="fas mr-2" :class="item.icon"></i>
+                  {{ item.name }}
+              </button>
+            </Tab>
+          </template>
+
+            <input v-focus autofocus v-if="showAddPageInput" @keyup.enter="addPage" v-model="newPageName" type="text" name="page" id="page" class="focus:ring-green-500 focus:border-green-500 block w-7/12 sm:text-sm border-gray-300 rounded placeholder:text-xs" placeholder="Enter page name...">
+
+
+            <button v-if="!showAddPageInput" @click="showAddPageInput = true" class="border border-dashed border-slate-300 dark:border-slate-600 w-7/12 py-1.5 text-slate-400 dark:text-slate-700 hover:scale-105 transition-all duration-300 rounded">+ New Page</button>
+        </TabList>
+
+        <TabPanels class="col-span-9 focus:outline-0 h-screen">
+            <template v-for="page in pages">
+                  <TabPanel class="focus:outline-0">
+                    <PanelArea :page="page" />
+                  </TabPanel>
+            </template>
+        </TabPanels>
+      </TabGroup>
+</section>
 
 </template>
 
--- src/components/PanelArea.vue
+++ src/components/PanelArea.vue
@@ -1,15 +1,33 @@
 <script setup lang="ts">
-import {onMounted, onUnmounted, ref} from 'vue'
-import {useDraggable, useMouse} from '@vueuse/core'
+import {computed, onMounted, onUnmounted, ref} from 'vue'
+import {useDraggable, useMouse, useElementBounding, clamp } from '@vueuse/core'
+
+import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
+
+defineProps({
+    page: {type: Object, required: true}
+})
 
 const el = ref<HTMLElement | null>(null)
 const handle = ref<HTMLElement | null>(null)
+const container = ref<HTMLElement | null>(null)
 
 // `style` will be a helper computed for `left: ?px; top: ?px;`
 const { x, y, style } = useDraggable(el, {
-  initialValue: { x: 40, y: 40 },
+    initialValue: { x: 126, y: 198 },
     handle
 })
+
+const { left, right, top, bottom } = useElementBounding(container);
+const { width: containerWidth, height: containerHeight } = useElementBounding(el);
+
+const restrictedX = computed(() =>
+  clamp(left.value, x.value, right.value - containerWidth.value)
+);
+const restrictedY = computed(() =>
+  clamp(top.value, y.value, bottom.value - containerHeight.value)
+);
+
 
 // resize functions
     const width = ref(300)
@@ -46,8 +64,73 @@ const { x, y, style } = useDraggable(el, {
 </script>
 
 <template>
-  <div class="graph-paper h-full w-full border">
-      <div ref="el" :style="style" style="position: fixed" id="panel" class="relative group">
+    <header class="flex justify-between">
+        <div></div>
+        <div class="space-x-5 flex items-center">
+            <div class="flex items-center flex-row">
+                <input type="date" class="focus:ring-green-500 focus:border-green-500 block sm:text-xs border-gray-300 rounded placeholder:text-xs" placeholder="Enter page name...">
+                <span class="text-slate-400 text-xs px-3">to</span>
+                <input type="date" class="focus:ring-green-500 focus:border-green-500 block sm:text-xs border-gray-300 rounded placeholder:text-xs" placeholder="Enter page name...">
+            </div>
+            <Menu as="div" className="relative flex">
+              <!--inline-block text-left-->
+                <button class="border border-gray-300 w-full justify-center rounded-l px-4 py-1.5 text-xs font-medium focus:outline-none">+ Add Panel</button>
+              <div>
+                <MenuButton
+                  class="border border-l-0 border-gray-300 inline-flex w-full justify-center rounded-r px-2 py-1.5 text-sm font-medium focus:outline-none"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                  </svg>
+
+                </MenuButton>
+              </div>
+
+              <transition
+                enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-in"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0"
+              >
+                <MenuItems
+                  class="absolute right-0 mt-8 w-56 origin-top-right divide-y divide-gray-100 rounded bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                >
+                  <div class="px-1 py-1">
+                    <MenuItem v-slot="{ active }">
+                      <button
+                        :class="[
+                          active ? 'bg-gray-300/30' : '',
+                          'group flex w-full items-center rounded px-2 py-2 text-xs',
+                        ]"
+                      >
+                        Edit
+                      </button>
+                    </MenuItem>
+                    <MenuItem v-slot="{ active }">
+                      <button
+                        :class="[
+                          active ? 'bg-gray-300/30' : 'text-gray-900',
+                          'group flex w-full items-center rounded px-2 py-2 text-xs',
+                        ]"
+                      >
+                        Edit Variables
+                      </button>
+                    </MenuItem>
+                  </div>
+                </MenuItems>
+              </transition>
+            </Menu>
+        </div>
+    </header>
+  <div ref="container" class="graph-paper min-h-[500px] w-full border mt-8">
+      <div ref="el" :style="{
+          userSelect: 'none',
+          position: 'fixed',
+          top: `${restrictedY}px`,
+          left: `${restrictedX}px`,
+        }" id="panel" class="relative group">
           <div class='resizable'>
             <div :style="{ width: `${width}px`, height: `${height}px` }" class="bg-white shadow-lg rounded-md min-w-[300px] min-h-[200px]">
 
